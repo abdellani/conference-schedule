59631a7475cfe4e282ae92b4b4f3de7b73a0898a
abdellani <abdellani@gmail.com>
2019-11-29 21:06:16 +0100

    Update the models and the seed file

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,2 +1,4 @@
 class Talk < ApplicationRecord
+  has_many :attendances
+  belongs_to :conference
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Administrator.create({name:"admin",email:"admin@admin.com",password:"abcdef"})
+User.create({name:"test",email:"test@test.com",password:"abcdef"})
+Conference.create({date:"01/01/2020",location:"Earth"})
+
+conference=Conference.all.first
+conference.talks.create({day:1,start_time:"00:00",end_time:"01:00",location:"A",description:"blabla"})
+talk=conference.talks.first
+user=User.all.first
+user.attendances.create({talk:talk})
