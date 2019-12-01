3a0365e5ff496d1d2af19f6873dbe213f8c348f6
abdellani <abdellani@gmail.com>
2019-12-01 23:50:36 +0100

    Integrate React

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'home_page#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope "/api" do
     resources :conferences, only: [:show, :index] do
