12d5fc81777e339d5191f413179d0fc6207c74b9
abdellani <abdellani@gmail.com>
2019-12-02 14:38:22 +0100

    Add bootstrap scss and fontawesome

--- app/javascript/packs/source/index.js
+++ app/javascript/packs/source/index.js
@@ -4,6 +4,10 @@ import {
   Switch,
   Route
 } from "react-router-dom"
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ConferencesIndex from "./conferences/"
 import ConferencesExhibitors from "./conferences/exhibitors"
 import ConferencesSponsors from "./conferences/sponsors"
@@ -11,6 +15,7 @@ import ConferencesTalksIndex from "./conferences/talks/index"
 import ConferencesTalksShow from "./conferences/talks/show"
 import ConferencesTalksQuestionsIndex from "./conferences/talks/questions/index"
 import ConferencesTalksAttendancesIndex from "./conferences/talks/attendances"
+
 const App = () =>
   <Router>
     <Switch>
