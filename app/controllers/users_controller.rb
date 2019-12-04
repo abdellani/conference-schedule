827d71a5ae632c0812dd36fb97b538a23da33a70
abdellani <abdellani@gmail.com>
2019-12-04 18:34:13 +0100

    Implement signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
-  def Create
-
-    render json: Conference.all.to_json(
-      only: [:id,:date,:location,:subject,:description]
-    )
+  skip_before_action :verify_authenticity_token, only: [:create]
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json:{
+          code: 200,
+        }
+      
+    else
+      render json:{
+        code: 400,
+        errors:@user.errors.messages
+      }
   end
-  private 
-    def user_params
-    end
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:name, :company, :role, :email, :mobile, :password, :bio)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  validates :name, presence:true
+  validates :company, presence:true
+  validates :role, presence:true
+  validates :email, presence:true
+  validates :mobile, presence:true
   has_secure_password
+  validates :bio, presence:true
   has_many :attendances
   has_many :questions
   has_many :notifications
+
 end
