f84350517f4518ac09ebb9e4f504b0be02358ae2
abdellani <abdellani@gmail.com>
2019-12-04 13:29:59 +0100

    Add exhibitors

--- app/javascript/packs/source/conferences/exhibitors.jsx
+++ app/javascript/packs/source/conferences/exhibitors.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react"
 import axios from "axios"
+import Navbar from "../navbar"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faBuilding, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 class ConferencesExhibitors extends Component {
   constructor(props) {
@@ -11,22 +14,83 @@ class ConferencesExhibitors extends Component {
   componentDidMount() {
     let { id } = this.props.match.params
     axios.get(`/api/conferences/${id}/exhibitors`).
-    then(response => this.setState({ exhibitors: response.data }))
+      then(response => this.setState({ exhibitors: response.data }))
   }
   render() {
-    let {exhibitors}= this.state
-    return(<div>
-      {
-        exhibitors.map(
-          e=>
-          <ul key={e.id}>
-            <li>{e.name}</li>
-            <li>{e.description}</li>
-            <li>{e.website}</li>
-          </ul>
-        )
-      }
-    </div>)
+    let { id } = this.props.match.params
+    let { exhibitors } = this.state
+    return (
+      <div>
+        <Navbar conference_id={id} />
+        <div className="bg-violet1 text-white font-weight-bolder px-3 py-2 pb-3">
+          Exhibitors
+        </div>
+        <div className="px-3">
+          {
+            exhibitors.map(
+              e =>
+                <div key={e.id} className="shadow-sm bg-white px-3 py-3 my-2">
+                  <div className="d-flex align-items-center">
+                    <div className="px-2 ">
+                      <h4>
+                        <FontAwesomeIcon icon={faBuilding} />
+                      </h4>
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center px-3 w-100">
+                      <div className="d-flex flex-column">
+                        <div>
+                          <small className="text-muted font-weight-bolder">
+                            Exhibitor {e.id}
+                          </small>
+                        </div>
+                        <div className="font-weight-bolder text-blue1">
+                          {e.name}
+                        </div>
+                      </div>
+                      <div className="rounded-pill p-1 bg-white shadow-sm ">
+                        <button
+                          className={
+                            ["show-day-schedule-button border-0",
+                              "bg-transparent", "collapsed"].join(" ")
+                          }
+                          data-toggle="collapse"
+                          data-target={`#exibitor${e.id}`}
+                        >
+                          <FontAwesomeIcon icon={faChevronDown} />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div id={`exibitor${e.id}`} className="collapse">
+                    <div>
+                      <div className="text-violet1 font-weight-bolder">
+                        About
+                    </div>
+                      <div>
+                        {e.description}
+                      </div>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <div className="px-2">
+                        <FontAwesomeIcon icon={faGlobe} />
+                      </div>
+                      <div className="d-flex flex-column px-2">
+                        <div>
+                          <small className="text-muted">
+                            Website
+                          </small>
+                        </div>
+                        <div className="font-weight-bolder text-violet1">
+                          {e.website}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            )
+          }
+        </div>
+      </div>)
   }
 }
 
