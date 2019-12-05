8b710e642483528ae10931f1ab45f1b7dc7796c7
abdellani <abdellani@gmail.com>
2019-12-06 00:26:04 +0100

    Add notification model validations

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,5 @@
 class Notification < ApplicationRecord
+  validates :content, presence: true, length: { minimum: 3, maximum: 100 }
+
   belongs_to :owner, class_name: :User,foreign_key: "user_id"
 end
