65f0f60731e2083ea80fab2ad1534246afebab29
abdellani <abdellani@gmail.com>
2019-12-06 00:18:27 +0100

    Add administrator model validations

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,3 +1,7 @@
 class Administrator < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { minimum: 3, maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX }
   has_secure_password
 end
