78a4bd76bc54432c924bd3d7e700397b3d5c2026
abdellani <abdellani@gmail.com>
2019-12-06 00:33:42 +0100

    Add attendance model validations

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,8 +1,11 @@
 class Attendance < ApplicationRecord
+  ROLES = ['speaker', 'moderator','participant']
+  validates :role,inclusion: {in: ROLES}
+  validates_uniqueness_of :role, scope: [:talk_id,:user_id]
+
   belongs_to :talk
   belongs_to :user
   scope :as_speaker, ->{ where(role: "speaker") }
   scope :as_moderator, ->{ where(role: "moderator") }
   scope :as_participant, ->{ where(role: "participant") }
-  validates_uniqueness_of :role, scope: [:talk_id,:user_id]
 end
