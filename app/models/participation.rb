b6bbfb0c78e9c4f1ed18260e7887e79425383044
abdellani <abdellani@gmail.com>
2019-12-06 00:25:05 +0100

    Add paritication model validations

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,6 +1,10 @@
 class Participation < ApplicationRecord
+  ROLES = ['exhibitor', 'sponsor']
+  validates :role,inclusion: {in: ROLES}
+
   belongs_to :company
   belongs_to :conference
+
   scope :as_sponsor,->{where(role:"sponsor")}
   scope :as_exhibitor,->{where(role:"exhibitor")}
 end
