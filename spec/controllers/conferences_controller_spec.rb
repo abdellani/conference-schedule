d5a873af810ee80a8dfa2aca9ce3d64e0b823125
abdellani <abdellani@gmail.com>
2019-12-14 01:58:25 +0100

    Add talks tests

--- spec/controllers/conferences_controller_spec.rb
+++ spec/controllers/conferences_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ConferencesController, type: :controller do
-  fixtures :users, :conferences
+  fixtures :users, :conferences, :talks
   describe "#index" do
     scenario "Should get code=401 if the user is not authenticated" do
       visit "/api/conferences/"
@@ -35,15 +35,29 @@ RSpec.describe ConferencesController, type: :controller do
     scenario "Should get code=200 with list of conferences if the user is authenticated" do
       page.set_rack_session(id: 1)
       visit "/api/conferences/1"
-      answer = { code: 200, data: 
-        { id: 1,
-          date: "2020-01-01",
-          location: "University X",
-          subject: "DevOps",
-          description: "Introduction to docker",
-          talks:[] },
-
-      }.to_json
+      answer = { code: 200, data: { id: 1,
+                                  date: "2020-01-01",
+                                  location: "University X",
+                                  subject: "DevOps",
+                                  description: "Introduction to docker",
+                                  talks: [
+        {
+          id: 1,
+          day: 1,
+          start_time: "2000-01-01T09:00:00.000Z",
+          end_time: "2000-01-01T10:00:00.000Z",
+          location: "A",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        },
+        {
+          id: 2,
+          day: 1,
+          start_time: "2000-01-01T10:00:00.000Z",
+          end_time: "2000-01-01T12:00:00.000Z",
+          location: "C",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        },
+      ] } }.to_json
       expect(page.body).to eq answer
     end
   end
